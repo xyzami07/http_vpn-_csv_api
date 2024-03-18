4db1569f89a22d4940bf438868c6d8512b91df41
ATIK ABRAR EMON <xyzami07@gmail.com>
2024-03-18 15:48:43 +0400

    init commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 import pandas as pd
+
+app = Flask(__name__)
+
 
 df = pd.read_csv('http://www.vpngate.net/api/iphone/', skiprows=1)
 
 df.rename(columns={"#HostName": "HostName"}, inplace=True)
 data_dict = df.to_dict()
-
-
-app = Flask("website")
 
 
 @app.route("/")
